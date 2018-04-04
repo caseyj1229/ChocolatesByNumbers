2f75a7f05efced57a1f13411a80e69502ef25d93
John Casey <31704237+caseyj1229@users.noreply.github.com>
2018-04-04 11:02:02 -0400

    Update ChocolatesByNumbers.java
    Cleaned up solution

--- ChocolatesByNumbers.java
+++ ChocolatesByNumbers.java
@@ -3,11 +3,9 @@ public class ChocolatesByNumbers {
         /*N is the number of chocolates available and thus the max amount able to be eaten
         * M is the number of the next chocolate to be eaten
         * The GCD represents the greeatest common divisor between N and M
-        * So, N*M/GCD will be the greatest number of chocolates that can be eaten before hitting a wrapper * M
+        * So, N*M/GCD will be the greatest number of chocolates that can be eaten before hitting a wrapper
         */
-        int chocolatesEaten = (N*M)/greatestCommonDivisor(N,M);
-        chocolatesEaten /= M;
-        return chocolatesEaten;
+        return (N/greatestCommonDivisor(N,M));
     }
 
     public static int greatestCommonDivisor(int a, int b){
